f89a1dc152c2f3c7afc124080225eda88a339215
AAGiron <alexandre.a.giron@gmail.com>
2025-03-22 15:04:56 -0300

    Add verify-nizkpop and standard FrodoKEM functions
    Given N (the number of iterations), now main.go executes N operations for FrodoKEM-640 Keygen, Keygen-nizkpop, Encaps, Decaps, Verify-nizkpop. Future work includes: new parameters (e.g., Frodo976; collect metrics for benchmarks; implement using go testing package.

--- main.go
+++ main.go
@@ -1,41 +1,107 @@
 // main.go
-
 package main
 
 /*
 #cgo CFLAGS: -I/home/<SEULOCAL>/zkpop-go/external/KEM-NIZKPoP/frodo-zkpop/frodo640/ -I/usr/include/
-#cgo LDFLAGS: -L/home/<SEULOCAL/projetos/Grupo_PQC/edital12PIBIC/zkpop-go/external/KEM-NIZKPoP/frodo-zkpop/frodo640/ -L/usr/lib/ -lfrodo -lssl -lcrypto 
+#cgo LDFLAGS: -L/home/<SEULOCAL>/zkpop-go/external/KEM-NIZKPoP/frodo-zkpop/frodo640/ -L/usr/lib/ -lfrodo -lssl -lcrypto 
 */
 import "C"
 
 import (
 	"fmt"
 	"log"
-	"zkpop-go/zkpop" // Make sure to import the package correctly
+	"bytes"
+	"zkpop-go/zkpop" 
 )
 
-func main() {
-	// Test KeyPair generation
-	fmt.Println("Generating keypair with zkpop...")
+//test FrodoKEM in N+1 iterations
+func testFrodoKEM(N int){
+        fmt.Println("Testing FrodoKEM...")
 
-//	pk, sk, zkpopProof, err := zkpop.KeyPair()
-	pk, sk, zkpopProof, err := zkpop.KeyPair()
+        //warmup
+        pk, sk, err := zkpop.KeyPairFrodo640()
+        if err != nil {
+                log.Fatalf("Error generating keypair: %v", err)
+        }
+
+        //test N keygens
+        for i := 0; i < N; i++  {
+                pk, sk, err = zkpop.KeyPairFrodo640()
+        }
+	
+	//warmup Encaps
+	ct, ss, err := zkpop.EncapsFrodo640(pk)
 	if err != nil {
-		log.Fatalf("Error generating keypair: %v", err)
+                log.Fatalf("Failed FrodoKEM encapsulation: %v", err)
+        }
+
+	//test N Encaps
+	for i := 0; i < N; i++  {
+                ct, ss, err = zkpop.EncapsFrodo640(pk)
+        }
+
+	//warmup Decaps
+	css, err := zkpop.DecapsFrodo640(ct, sk)
+	if err != nil || !bytes.Equal(ss,css) {
+                log.Fatalf("Failed FrodoKEM decapsulation.")
+        }
+
+	//test N Decaps
+	for i := 0; i < N; i++  {
+                css, err = zkpop.DecapsFrodo640(ct, sk)
+        }
+
+}
+
+
+//test FrodoKEM-NIZKPoP in N+1 iterations
+func testFrodoKEMNIZKPoP(N int){
+  	fmt.Println("Testing FrodoKEM-NIZKPoP...")
+
+	//warmup
+        pk, _, zkpopProof, err := zkpop.KeyPairFrodo640NIZKPoP()
+        if err != nil {
+                log.Fatalf("Error generating keypair: %v", err)
+        }
+
+	//test N keygens
+	for i := 0; i < N; i++  {
+        	pk, _, zkpopProof, err = zkpop.KeyPairFrodo640NIZKPoP()
 	}
 
-	fmt.Printf("Public Key: %x\n", pk)
-	fmt.Printf("Secret Key: %x\n", sk)
-	fmt.Printf("Zero-Knowledge Proof: %x\n", zkpopProof)
+        //fmt.Printf("Public Key: %x\n", pk)
+        //fmt.Printf("Secret Key: %x\n", sk)
+        //fmt.Printf("Zero-Knowledge Proof: %x\n", zkpopProof)
 
-	// Test zkpop verification
-	//fmt.Println("Verifying zkpop...")
+	//warmup
+	valid := zkpop.VerifyFrodo640ZKPop(pk, zkpopProof)
+        if !valid {
+                log.Fatalf("Error verifying ZKPoP: %v", err)
+	}
 
-	/*isValid := zkpop.VerifyZKPop(pk, zkpopProof) // Call zkpop.VerifyZKPop correctly
-	if isValid {
-		fmt.Println("zkpop verification successful!")
-	} else {
-		fmt.Println("zkpop verification failed.")
-	}*/
+	//test N verifications
+	for i := 0; i < N; i++  {
+		valid = zkpop.VerifyFrodo640ZKPop(pk, zkpopProof) 
+	}
+}
+
+//test FrodoKEM-NIZKPoP in N+1 iterations
+func testKyber(N int){
+	fmt.Println("Testing Kyber... Not implemented yet")
+}
+
+
+func main() {
+	N := 10
+	fmt.Printf("Testing %d iterations for each algorithm...\n", N)
+
+	//Frodo-KEM
+	testFrodoKEM(N)
+	testFrodoKEMNIZKPoP(N)
+
+	//TODO: Kyber
+	
+
+	fmt.Println("End of testing.")
 }
 
--- zkpop/zkpop.go
+++ zkpop/zkpop.go
@@ -16,9 +16,9 @@ import (
 )
 
 
-//Frodo part
-func KeyPair() ([]byte, []byte, []byte, error) {
-        fmt.Println(C.CRYPTO_ALGNAME)
+//Frodo640-Keypair with NIZKPoP
+func KeyPairFrodo640NIZKPoP() ([]byte, []byte, []byte, error) {
+        //fmt.Println(C.CRYPTO_ALGNAME)
 	pk := make([]byte, C.CRYPTO_PUBLICKEYBYTES)
         sk := make([]byte, C.CRYPTO_SECRETKEYBYTES)
         var zkpop *C.uint8_t
@@ -38,35 +38,12 @@ func KeyPair() ([]byte, []byte, []byte, error) {
         return pk, sk, zkpopGo, nil
 }
 
-
-
-
-// KeyPair generates a keypair and proof of knowledge (zkpop).
-/*func KeyPair() ([]byte, []byte, []byte, error) {
-	pk := make([]byte, C.KYBER_INDCPA_PUBLICKEYBYTES)
-	sk := make([]byte, C.KYBER_INDCPA_SECRETKEYBYTES)
-	var zkpop *C.uint8_t
-	var zkpopSize C.size_t
-
-	ret := C.crypto_kem_keypair_nizkpop((*C.uint8_t)(unsafe.Pointer(&pk[0])),
-		(*C.uint8_t)(unsafe.Pointer(&sk[0])),
-		&zkpop, &zkpopSize)
-
-	if ret != 0 {
-		return nil, nil, nil, fmt.Errorf("failed to generate keypair")
-	}
-
-	zkpopGo := C.GoBytes(unsafe.Pointer(zkpop), C.int(zkpopSize))
-	C.free(unsafe.Pointer(zkpop)) // Free zkpop allocated in C
-
-	return pk, sk, zkpopGo, nil
+func VerifyFrodo640ZKPop(pk []byte, zkpop []byte) bool {
+        ret := C.crypto_nizkpop_verify_Frodo640((*C.uchar)(unsafe.Pointer(&pk[0])),
+                (*C.uchar)(unsafe.Pointer(&zkpop[0])),
+                C.ulong(len(zkpop))) //we could set a 'CRYPTO_NIZKPOPBYTES' in the .h API file...
+        return ret == 0
 }
 
-// VerifyZKPop verifies the proof of knowledge.
-func VerifyZKPop(pk []byte, zkpop []byte) bool {
-	ret := C.crypto_nizkpop_verify((*C.uchar)(unsafe.Pointer(&pk[0])),
-		(*C.uchar)(unsafe.Pointer(&zkpop[0])),
-		C.ulong(len(zkpop)))
-	return ret == 0
-}
-*/
+
+
