7e03710df8a1b2b9fb57fa238e69dd80fc31bc21
Gabriela S. R. <86333622+gabsxramos@users.noreply.github.com>
2025-05-04 12:17:01 -0300

    Update zkpop.go

--- zkpop/zkpop.go
+++ zkpop/zkpop.go
@@ -1,11 +1,11 @@
-// zkpop/zkpop.go
+// zkpop/zkpop.go — Frodo1344 NIZKPoP
 package zkpop
 
 /*
 #cgo CFLAGS: -I../external/KEM-NIZKPoP/frodo-zkpop/src
-#cgo LDFLAGS: -L../external/KEM-NIZKPoP/frodo-zkpop/frodo640 -lfrodo
+#cgo LDFLAGS: -L../external/KEM-NIZKPoP/frodo-zkpop/frodo1344 -lfrodo
 
-#include "api_frodo640.h"
+#include "api_frodo1344.h"
 #include <stdint.h>
 #include <stdlib.h>
 #include <openssl/evp.h>
@@ -13,44 +13,34 @@ package zkpop
 */
 import "C"
 
-import "C"
-
 import (
-        "fmt"
-        "unsafe"
+	"fmt"
+	"unsafe"
 )
 
-
-//Frodo640-Keypair with NIZKPoP
-func KeyPairFrodo640NIZKPoP() ([]byte, []byte, []byte, error) {
-        //fmt.Println(C.CRYPTO_ALGNAME)
+func KeyPairFrodo1344NIZKPoP() ([]byte, []byte, []byte, error) {
 	pk := make([]byte, C.CRYPTO_PUBLICKEYBYTES)
-        sk := make([]byte, C.CRYPTO_SECRETKEYBYTES)
-        var zkpop *C.uint8_t
-        var zkpopSize C.size_t
+	sk := make([]byte, C.CRYPTO_SECRETKEYBYTES)
+	var zkpop *C.uint8_t
+	var zkpopSize C.size_t
 
-        ret := C.crypto_kem_keypair_nizkpop_Frodo640((*C.uint8_t)(unsafe.Pointer(&pk[0])),
-                (*C.uint8_t)(unsafe.Pointer(&sk[0])),
-                &zkpop, &zkpopSize)
+	ret := C.crypto_kem_keypair_nizkpop_Frodo1344((*C.uint8_t)(unsafe.Pointer(&pk[0])),
+		(*C.uint8_t)(unsafe.Pointer(&sk[0])),
+		&zkpop, &zkpopSize)
 
-        if ret != 0 {
-                return nil, nil, nil, fmt.Errorf("failed to generate keypair")
-        }
+	if ret != 0 {
+		return nil, nil, nil, fmt.Errorf("failed to generate keypair")
+	}
 
-        zkpopGo := C.GoBytes(unsafe.Pointer(zkpop), C.int(zkpopSize))
-        C.free(unsafe.Pointer(zkpop)) // Free zkpop allocated in C
+	zkpopGo := C.GoBytes(unsafe.Pointer(zkpop), C.int(zkpopSize))
+	C.free(unsafe.Pointer(zkpop))
 
-        return pk, sk, zkpopGo, nil
+	return pk, sk, zkpopGo, nil
 }
 
-func VerifyFrodo640ZKPop(pk []byte, zkpop []byte) bool {
-        ret := C.crypto_nizkpop_verify_Frodo640((*C.uchar)(unsafe.Pointer(&pk[0])),
-                (*C.uchar)(unsafe.Pointer(&zkpop[0])),
-                C.ulong(len(zkpop))) //we could set a 'CRYPTO_NIZKPOPBYTES' in the .h API file...
-        return ret == 0
+func VerifyFrodo1344ZKPop(pk []byte, zkpop []byte) bool {
+	ret := C.crypto_nizkpop_verify_Frodo1344((*C.uchar)(unsafe.Pointer(&pk[0])),
+		(*C.uchar)(unsafe.Pointer(&zkpop[0])),
+		C.ulong(len(zkpop)))
+	return ret == 0
 }
-
-
-
-
-
