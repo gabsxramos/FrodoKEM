8d5357d386cc072b88c6910c9dcc215242877f9f
Gabriela S. R. <86333622+gabsxramos@users.noreply.github.com>
2025-05-04 12:17:21 -0300

    Update frodoKEM.go

--- zkpop/frodoKEM.go
+++ zkpop/frodoKEM.go
@@ -1,62 +1,54 @@
-// FrodoKEM original bindings, just for comparison purposes
+// FrodoKEM-1344 bindings
 package zkpop
 
 /*
-#include "api_frodo640.h"
+#include "api_frodo1344.h"
 #include <stdint.h>
 #include <stdlib.h>
-#include <openssl/evp.h> //from OpenSSL 1.1.1
+#include <openssl/evp.h>
 #include <openssl/aes.h>
 */
 import "C"
 
 import (
-        "fmt"
-        "unsafe"
+	"fmt"
+	"unsafe"
 )
 
-//binding for crypto_kem_keypair_Frodo640
-func KeyPairFrodo640()([]byte, []byte, error){
+func KeyPairFrodo1344() ([]byte, []byte, error) {
 	pk := make([]byte, C.CRYPTO_PUBLICKEYBYTES)
-        sk := make([]byte, C.CRYPTO_SECRETKEYBYTES)
+	sk := make([]byte, C.CRYPTO_SECRETKEYBYTES)
 
-	ret := C.crypto_kem_keypair_Frodo640((*C.uint8_t)(unsafe.Pointer(&pk[0])),
-                (*C.uint8_t)(unsafe.Pointer(&sk[0])))
+	ret := C.crypto_kem_keypair_Frodo1344((*C.uint8_t)(unsafe.Pointer(&pk[0])),
+		(*C.uint8_t)(unsafe.Pointer(&sk[0])))
 
 	if ret != 0 {
-                return nil, nil, fmt.Errorf("failed to generate keypair")
-        }
+		return nil, nil, fmt.Errorf("failed to generate keypair")
+	}
 	return pk, sk, nil
 }
 
-//Encapsulation for a given public key pk
-//Returns a ciphertext ct and a 16-byte shared secret ss
-func EncapsFrodo640(pk []byte)([]byte, []byte, error){
-//	crypto_kem_enc_Frodo640
-//	(unsigned char *ct, unsigned char *ss, const unsigned char *pk)
+func EncapsFrodo1344(pk []byte) ([]byte, []byte, error) {
 	ss := make([]byte, C.CRYPTO_BYTES)
 	ct := make([]byte, C.CRYPTO_CIPHERTEXTBYTES)
 
-	ret := C.crypto_kem_enc_Frodo640((*C.uint8_t)(unsafe.Pointer(&ct[0])),
-					   (*C.uint8_t)(unsafe.Pointer(&ss[0])),
-					   (*C.uint8_t)(unsafe.Pointer(&pk[0])))
+	ret := C.crypto_kem_enc_Frodo1344((*C.uint8_t)(unsafe.Pointer(&ct[0])),
+		(*C.uint8_t)(unsafe.Pointer(&ss[0])),
+		(*C.uint8_t)(unsafe.Pointer(&pk[0])))
 	if ret != 0 {
-                return nil, nil, fmt.Errorf("failed to encaps")
-        }
-        return ct, ss, nil
+		return nil, nil, fmt.Errorf("failed to encaps")
+	}
+	return ct, ss, nil
 }
 
-//Given a ciphertext ct and a private key sk
-//returns a candidate shared secret css
-func DecapsFrodo640(ct []byte, sk []byte)([]byte, error){
-	//crypto_kem_dec_Frodo640
+func DecapsFrodo1344(ct []byte, sk []byte) ([]byte, error) {
 	css := make([]byte, C.CRYPTO_BYTES)
 
-        ret := C.crypto_kem_dec_Frodo640((*C.uint8_t)(unsafe.Pointer(&css[0])),
-                                           (*C.uint8_t)(unsafe.Pointer(&ct[0])),
-                                           (*C.uint8_t)(unsafe.Pointer(&sk[0])))
-        if ret != 0 {
-                return nil, fmt.Errorf("failed to perform decapsulation")
-        }
-        return css, nil
+	ret := C.crypto_kem_dec_Frodo1344((*C.uint8_t)(unsafe.Pointer(&css[0])),
+		(*C.uint8_t)(unsafe.Pointer(&ct[0])),
+		(*C.uint8_t)(unsafe.Pointer(&sk[0])))
+	if ret != 0 {
+		return nil, fmt.Errorf("failed to perform decapsulation")
+	}
+	return css, nil
 }
